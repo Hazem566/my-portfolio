c2cf7a6deb2e4255c5f10487ec82483cd3010afa
HAZEM <haznmhmwd535@gmail.com>
2024-04-04 15:15:52 +0200

    update portfolio

--- script.js
+++ script.js
@@ -1,40 +1,39 @@
 // icon => nic
 // link => active__item
 /* ----------- target elements ----------- */
-const dot = document.querySelector(".dot");
-
 const header = document.getElementById("header");
 const linksContainer = document.querySelector(".menu");
 const links = document.querySelector(".links");
 const navLinks = document.querySelectorAll(".nav__item");
 const menuBtn = document.querySelector(".icon");
-
 const sections = document.querySelectorAll("section");
-
-
+const img = document.querySelector(".img__border");
+const bannerText = document.querySelector(".text__container");
+const curser = document.querySelector(".curser");
 /* -------------- event listeners -------------- */
-window.addEventListener("DOMContentLoaded", () => {
-    setInterval(() => {
-        blink(dot);
-    }, 1000);
-});
-window.addEventListener("resize", () => {
-    setHeight();
-    if(window.innerWidth > 992) {
-        linksContainer.style.height = "min-content";
-        menuBtn.classList.remove("nic");
-    } else {
-        linksContainer.style.height = "0";
-        menuBtn.classList.remove("nic");
-    }
-});
+window.addEventListener("DOMContentLoaded", reloadPage);
+window.addEventListener("resize", resizePage);
 menuBtn.addEventListener("click", toggleNav);
 navLinks.forEach(link=>link.addEventListener("click", navigateBetweenSections));
+observeItems(img, "slideTop", "add");
+observeItems(bannerText, "slideTop-2", "add");
 /* -------------- functions -------------- */
+// window event functions
+function reloadPage() {
+    setHeight();
+    blink(curser);
+}
+function resizePage() {
+    setHeight();
+    backNavTodefault();
+}
 
+// navbar functions
 function blink(ele) {
-    if(ele.classList.contains("blink")) ele.classList.remove("blink");
-    else ele.classList.add("blink");
+    setInterval(() => {
+        if(ele.classList.contains("blink")) ele.classList.remove("blink");
+        else ele.classList.add("blink");
+    }, 800);
 }
 function toggleNav() {
     if(window.innerWidth <= 992) {
@@ -75,10 +74,40 @@ function navigateBetweenSections(e) {
         });
     }
 }
+function backNavTodefault() {
+    if(window.innerWidth > 992) {
+        linksContainer.style.height = "min-content";
+        menuBtn.classList.remove("nic");
+    } else {
+        linksContainer.style.height = "0";
+        menuBtn.classList.remove("nic");
+    }
+}
+
+// set sections height
 function setHeight() {
     let headerHeight = header.offsetHeight;
     sections.forEach(sec => {
         sec.style.height = `calc(100dvh - ${headerHeight}px)`;
     });
 }
-setHeight();
+
+// set inter sections observers
+function observeItems(item, className, classState) {
+    let itemObserve = new IntersectionObserver(entrtis => {
+        entrtis.forEach(entry => {
+            if(classState === "add") {
+                if(entry.isIntersecting) {
+                     entry.target.classList.add(className);
+                }
+            } else {
+                if(entry.isIntersecting) {
+                    entry.target.classList.toggle(className);
+                }
+            }
+        });
+    });
+    return itemObserve.observe(item);
+}
+
+
